51a85c865fc932fc49abf6140d582f2649a3ccc1
Sandro Lain <sandrolain@outlook.com>
2023-08-26 22:25:41 +0200

    Added GRPC lib

---
+++ pkg/grpclib/grpclib.go
@@ -0,0 +1,31 @@
+package grpclib
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/sandrolain/gomsvc/pkg/svc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type ServerOptions struct {
+	Port    int
+	Desc    *grpc.ServiceDesc
+	Handler interface{}
+}
+
+func CreateServer(opts ServerOptions) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", opts.Port))
+	if err != nil {
+		return err
+	}
+	s := grpc.NewServer()
+	s.RegisterService(opts.Desc, opts.Handler)
+	reflection.Register(s)
+	svc.Logger().Info("start gRPC server", "addr", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		return err
+	}
+	return nil
+}

bce014b849de04a3167e67859fe598a8ba427dae
Sandro Lain <sandrolain@outlook.com>
2024-09-01 15:48:15 +0200

    [feat] Added cryptolib with AES functions

---
+++ pkg/cryptolib/cryptlib.go
@@ -0,0 +1,47 @@
+package cryptolib
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+)
+
+// EncryptAES encrypts the plaintext using the provided key
+func EncryptAES(plainText, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	cipherText := make([]byte, aes.BlockSize+len(plainText))
+	iv := cipherText[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(cipherText[aes.BlockSize:], plainText)
+
+	return cipherText, nil
+}
+
+// DecryptAES decrypts the ciphertext using the provided key
+func DecryptAES(cipherText, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(cipherText) < aes.BlockSize {
+		return nil, err
+	}
+
+	iv := cipherText[:aes.BlockSize]
+	cipherText = cipherText[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(cipherText, cipherText)
+
+	return cipherText, nil
+}
---
+++ pkg/cryptolib/cryptolib_test.go
@@ -0,0 +1,55 @@
+package cryptolib
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/rand"
+	"testing"
+)
+
+func TestEncryptAES(t *testing.T) {
+	keyLengths := []int{16, 24, 32} // AES-128, AES-192, AES-256
+
+	for _, keyLength := range keyLengths {
+		key := make([]byte, keyLength)
+		if _, err := rand.Read(key); err != nil {
+			t.Fatalf("Failed to generate key: %v", err)
+		}
+
+		plainText := []byte("Test plaintext")
+		cipherText, err := EncryptAES(plainText, key)
+		if err != nil {
+			t.Fatalf("EncryptAES failed for key length %d: %v", keyLength, err)
+		}
+
+		if len(cipherText) <= aes.BlockSize {
+			t.Fatalf("Ciphertext too short for key length %d: %d", keyLength, len(cipherText))
+		}
+	}
+}
+
+func TestDecryptAES(t *testing.T) {
+	keyLengths := []int{16, 24, 32} // AES-128, AES-192, AES-256
+
+	for _, keyLength := range keyLengths {
+		key := make([]byte, keyLength)
+		if _, err := rand.Read(key); err != nil {
+			t.Fatalf("Failed to generate key: %v", err)
+		}
+
+		plainText := []byte("Test plaintext")
+		cipherText, err := EncryptAES(plainText, key)
+		if err != nil {
+			t.Fatalf("EncryptAES failed for key length %d: %v", keyLength, err)
+		}
+
+		decryptedText, err := DecryptAES(cipherText, key)
+		if err != nil {
+			t.Fatalf("DecryptAES failed for key length %d: %v", keyLength, err)
+		}
+
+		if !bytes.Equal(plainText, decryptedText) {
+			t.Fatalf("Decrypted text does not match plaintext for key length %d. Got %s, want %s", keyLength, decryptedText, plainText)
+		}
+	}
+}
